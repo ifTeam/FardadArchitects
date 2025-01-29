66dee9982b85b7e885a2a5eab55da67e84546cca
javad-fallah-official <javad.fallah.official@gmail.com>
2025-01-29 03:24:08 +0000

    fixed links in home.html

--- MainSite/urls.py
+++ MainSite/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import *
+
 
 urlpatterns = [
     path('', home, name='home'),
+    path('projects/', projects, name='projects'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
 ]
--- MainSite/views.py
+++ MainSite/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')  # No need for app-specific subfolders
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
