7114e2784f417a06e5a11f9802645b1b4183e828
javad-fallah-official <javad.fallah.official@gmail.com>
2025-02-17 15:46:26 +0000

    aded gallery to projects page

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views
+from .views import projects_view
 
 urlpatterns = [
-    path('', views.project_gallery, name='projects'),  # Change 'project_gallery' to 'projects'
-    path('<int:project_id>/', views.project_detail, name='project_detail'),  # Keep the detail URL
+    path('projects/', projects_view, name='projects'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Project
+# projects/views.py
+import os
+from django.conf import settings
+from django.shortcuts import render
 
-def project_gallery(request):
-    # Fetch all projects
-    projects = Project.objects.all()
-    return render(request, 'projects.html', {'projects': projects})
-
-def project_detail(request, project_id):
-    # Fetch a specific project by ID
-    project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'project_detail.html', {'project': project})
-
-
+def projects_view(request):
+    projects_dir = os.path.join(settings.STATIC_ROOT, 'images', 'projects')
+    projects = []
+    
+    # Get all project folders
+    for folder in os.listdir(projects_dir):
+        if os.path.isdir(os.path.join(projects_dir, folder)):
+            # Extract project name from folder name (remove number prefix)
+            project_name = ' '.join(folder.split('-')[1:])
+            project_data = {
+                'name': project_name,
+                'folder': folder,
+                'image': f'images/projects/{folder}/main.jpg',
+                'date': '2000'  # Default date
+            }
+            projects.append(project_data)
+    
+    # Sort projects by folder number
+    projects.sort(key=lambda x: int(x['folder'].split('-')[0]))
+    
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
