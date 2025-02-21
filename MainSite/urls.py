fd69027542519b0a3c09f68944dd428f842541b9
javad-fallah-official <javad.fallah.official@gmail.com>
2025-02-21 18:44:24 +0000

    added about elements

--- MainSite/urls.py
+++ MainSite/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
 from .views import *  # Assuming you have the views for 'home', 'contact', 'about', etc.
+from .views import about_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('projects/', include('projects.urls')),  # This includes the URLs of the projects app
     path('contact/', contact, name='contact'),
-    path('about/', about, name='about'),
+    path('about/', about_view, name='about'),
 ]
--- MainSite/views.py
+++ MainSite/views.py
@@ -11,3 +11,6 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def about_view(request):
+    return render(request, 'about.html')
